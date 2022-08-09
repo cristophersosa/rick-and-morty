bdaea0043f9a7dee9c738e20f96c8622b172f0da
cristophersosa <38702620+cristophersosa@users.noreply.github.com>
2022-08-09 01:04:19 -0500

    Final version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,61 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-test';
+  loading = false;
+  data = [{
+    image: '',
+    name: '',
+    species: '',
+    status: '',
+    gender: '',
+    location: {
+      name: '',
+      url: ''
+    },
+    origin: {
+      name: '',
+      url: '',
+    },
+  }];
+
+  modalData = {
+    image: '',
+    name: '',
+    species: '',
+    status: '',
+    gender: '',
+    location: {
+      name: '',
+      url: ''
+    },
+    origin: {
+      name: '',
+      url: '',
+    },
+  };
+
+
+  constructor(private modalService: NgbModal) {
+    this.data = [];
+  }
+
+  ngOnInit(): void {
+    this.getData();
+  }
+
+  getData(value: string = '') {
+    this.loading= true;
+    fetch('https://rickandmortyapi.com/api/character/?name='+value).then( (response) => {
+        return response.json();
+      }).then( (data) =>{
+        this.data = data.results.slice(0, 12);
+        this.loading=false;
+      })
+  }
+
+  open(content: any, data: any) {
+    this.modalData = data;
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title', size: 'lg', backdrop: 'static'})
+  }
+
 }
